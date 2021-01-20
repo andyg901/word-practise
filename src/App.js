08b00180d2e727bebe91a0e537b7a79fbe55e573
andyg901 <andrewgrgn@protonmail.com>
2021-01-20 23:20:23 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+          <span className="navbar-text fs-4">Words practice</span>
+        </div>
+      </nav>
+      <main>
+        
+      </main>
+    </>
+  );
+}
+
+export default App;
